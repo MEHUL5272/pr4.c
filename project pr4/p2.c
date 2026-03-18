5d48dc82f0d07e295165c8a1c122e7ef7bb3274a
MEHUL5272 <thakormehul84666@gmail.com>
2026-03-18 09:40:24 +0530

    Add files via upload

---
+++ project pr4/p2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,no=11;
+    for(i = 1; i <= 4; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ",no);
+            no++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+   
---
+++ project pr4/p5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= 5 - i; j++)
+        {
+            printf("  ");
+        }
+        for(k = 5 - i + 1; k <= 5; k++)
+        {
+            printf("%d ", k);
+        }
+
+        for(k = 5 - 1; k >= 5 - i + 1; k--)
+        {
+            printf("%d ", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
